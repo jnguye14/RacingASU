71d552e8c2e6fbae034861cd63506fc92bcc4379
Jordan Nguyen <Jordan.N.Nguyen@asu.edu>
2014-03-07 13:37:59 -0700

    Reorganizing Folders
    Hopefully nothing was corrupt

--- Assets/Scripts/ArrowScript.js
+++ Assets/Scripts/ArrowScript.js
@@ -4,8 +4,8 @@ var destination : GameObject;
 function Update()
 {
 	transform.position = player.transform.position;
-	//transform.position.y += 1;
 	transform.position.y = GameObject.Find("Main Camera").transform.position.y;
+	transform.position.y += 4.0;
 	var lookTarget = Vector3(destination.transform.position.x, destination.transform.position.y, destination.transform.position.z);
 	lookTarget.y -= 180;
 	transform.LookAt(lookTarget);
